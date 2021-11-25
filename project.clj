24d4ceec2fd77a9e273865f5f0f009b3908c0a73
Max Penet <max.penet@exoscale.ch>
2021-11-25 09:19:18 +0100

    update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,20 @@
-(defproject exoscale/lingo "1.0.0-alpha1-SNAPSHOT"
+(defproject com.exoscale/lingo "1.0.0-alpha1-SNAPSHOT"
   :description ""
   :url "https://github.com/exoscale/lingo"
   :dependencies [[org.clojure/clojure "1.10.2"]]
-  :global-vars {*warn-on-reflection* true})
+  :global-vars {*warn-on-reflection* true}
+  :deploy {:repositories [["private" {:url "s3p://exo-artifacts/releases"
+                                      :no-auth true
+                                      :sign-releases false}]]}
+  :repositories [["exoscale" {:url "https://artifacts.exoscale.ch"}]]
+  :deploy-repositories [["releases" {:url           "s3p://exo-artifacts/releases"
+                                     :no-auth       true
+                                     :sign-releases false}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
