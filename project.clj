f1e61c0dd2f5627d1f1a5fd470dff55a78646f34
Max Penet <max.penet@exoscale.ch>
2021-11-25 14:08:57 +0100

    make it deployable

--- project.clj
+++ project.clj
@@ -1,19 +1,20 @@
 (defproject com.exoscale/lingo "1.0.0-alpha1-SNAPSHOT"
-  :description ""
+
+  :description "exoscale spec explain utils"
+
+  :license {:name "ISC"}
+
   :url "https://github.com/exoscale/lingo"
+
   :dependencies [[org.clojure/clojure "1.10.2"]]
+
   :global-vars {*warn-on-reflection* true}
 
-  :repositories [["exoscale" {:url "https://artifacts.exoscale.ch"}]]
-  :deploy-repositories [["releases" {:url "s3p://exo-artifacts/releases"
-                                     :no-auth true
-                                     :sign-releases false}]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "--no-sign"]
-                  ["deploy"]
+                  ["deploy" "clojars"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]]
-  :plugins [[exoscale/sos-wagon-private "1.3.2-exoscale8"]])
+                  ["vcs" "push"]])
