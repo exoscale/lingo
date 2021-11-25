3c5a94d44f393bae3a8e625c5d0eb877cbfb460b
Max Penet <max.penet@exoscale.ch>
2021-11-25 09:15:46 +0100

    set to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/lingo "0.1.0-SNAPSHOT"
+(defproject exoscale/lingo "1.0.0-alpha1-SNAPSHOT"
   :description ""
   :url "https://github.com/exoscale/lingo"
   :dependencies [[org.clojure/clojure "1.10.2"]]
